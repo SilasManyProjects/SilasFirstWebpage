4234a0d8332cec8a3d22262e88531f41af44b7db
SilasManyProjects <mvinmoving@gmail.com>
2026-02-04 12:11:24 -0500

    Update script.js
    javascript minified

--- script.js
+++ script.js
@@ -1,40 +1,5 @@
-// Grab elements
-const modal = document.getElementById("imageModal");
-const modalImage = document.getElementById("modalImage");
-const modalDescription = document.getElementById("modalDescription");
-const galleryImages = document.querySelectorAll(".gallery img");
-const closeBtn = document.querySelector(".close");
-
-// Add click event to all gallery images
-galleryImages.forEach(img => {
-    img.onclick = function () {
-        modal.style.display = "flex";
-        modalImage.src = this.src;
-        modalDescription.innerHTML = this.title;
-        modalImage.style.animation = "zoomIn 0.3s ease";
-    }
-});
-
-// Close modal when clicking the 'X'
-closeBtn.onclick = function () {
-    closeModal();
-}
-
-// Close modal when clicking the dark background
-modal.onclick = function (event) {
-    if (event.target === modal) {
-        closeModal();
-    }
-}
-
-function closeModal() {
-    modal.style.display = "none";
-}
-
-// Disable Right Click (Context Menu)
-function disableRightClick() {
-    document.addEventListener("contextmenu", function (e) {
-        e.preventDefault();
-    });
-}
+const modal=document.getElementById("imageModal");const modalImage=document.getElementById("modalImage");const modalDescription=document.getElementById("modalDescription");const galleryImages=document.querySelectorAll(".gallery img");const closeBtn=document.querySelector(".close");galleryImages.forEach(img=>{img.onclick=function(){modal.style.display="flex";modalImage.src=this.src;modalDescription.innerHTML=this.title;modalImage.style.animation="zoomIn 0.3s ease";}});closeBtn.onclick=function(){closeModal();}
+modal.onclick=function(event){if(event.target===modal){closeModal();}}
+function closeModal(){modal.style.display="none";}
+function disableRightClick(){document.addEventListener("contextmenu",function(e){e.preventDefault();});}
 disableRightClick();
