671be94e908c797d64e911b0a9787e82674f3949
SilasManyProjects <mvinmoving@gmail.com>
2026-02-04 19:51:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const modal=document.getElementById("imageModal");const modalImage=document.getElementById("modalImage");const modalDescription=document.getElementById("modalDescription");const galleryImages=document.querySelectorAll(".gallery img");const closeBtn=document.querySelector(".close");galleryImages.forEach(img=>{img.onclick=function(){modal.style.display="flex";modalImage.src=this.src;modalDescription.innerHTML=this.title;modalImage.style.animation="zoomIn 0.3s ease";}});closeBtn.onclick=function(){closeModal();}
 modal.onclick=function(event){if(event.target===modal){closeModal();}}
 function closeModal(){modal.style.display="none";}
-function disableRightClick(){document.addEventListener("contextmenu",function(e){e.preventDefault();});}
+const musicBtn=document.getElementById('musicBtn');let audio=null;let isPlaying=false;musicBtn.onclick=function(){if(!audio){audio=new Audio('music.mp3');audio.loop=true;audio.volume=0.5;}
+if(isPlaying){audio.pause();musicBtn.innerHTML="► Play Music";musicBtn.style.background="linear-gradient(to right, #380052, #1f003b)";}else{audio.play();musicBtn.innerHTML="❚❚ Pause Music";musicBtn.style.background="#5e008a";}
+isPlaying=!isPlaying;};function disableRightClick(){document.addEventListener("contextmenu",function(e){e.preventDefault();});}
 disableRightClick();
